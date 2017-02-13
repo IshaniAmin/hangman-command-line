a0dde21092f10428bc79460877697adad5def207
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-02-13 18:10:05 -0500

    game now can restart

--- game.js
+++ game.js
@@ -14,13 +14,17 @@ function askLetter(){
     {
     type: "input",
     name: "guess",
-    message: "What letter do you guess? To restart game type 'resart'"},
+    message: "What letter do you guess? To restart game type 'restart'"},
     ]).then(function(data){
         if (data.guess != 'restart') {
             wordObject.updateLetter(data.guess);
-
             console.log(wordObject.display());
-
+            askLetter();
+        } else if (data.guess == 'restart') {
+            wordObject = new Word(animals[Math.floor(Math.random()*animals.length)])
+            wordObject.makeAndPushLettersIntoWord();
+            wordObject.updateLetter(data.guess);
+            console.log(wordObject.display());
             askLetter();
         }
 
