5da30550f5958f6514a4429f20132e7ebec2bde2
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-02-14 16:39:41 -0500

    fixing counter

--- game.js
+++ game.js
@@ -3,13 +3,17 @@ var Word = require('./Word.js');
 
 var animals = ['dog', 'cat', 'parrot', 'hamster', 'fish', 'monkey', 'elephant'];
 
+// randomly chooses a word to play from the array
 var wordToPlay = animals[Math.floor(Math.random()*animals.length)];
 
-var guesses = 5;
+// initiating how many guesses the user has
+var guesses = 4;
 
+// displays when game begins
 var wordObject = new Word(wordToPlay);
 wordObject.makeAndPushLettersIntoWord();
-console.log(guesses + " guesses left " + wordObject.display());
+console.log("Here is your word to guess, you have 5 guesses.");
+console.log(wordObject.display());
 
 function askLetter(){
     inquirer.prompt([
@@ -20,23 +24,30 @@ function askLetter(){
     ]).then(function(data){
         if (data.guess != 'restart') {
             wordObject.updateLetter(data.guess);
-            console.log(guesses + " guesses left " + wordObject.display());
+            console.log("Guesses left: " + guesses);
+            console.log(wordObject.display());
             guesses--;
             askLetter();
+
+            if (guesses == 0) {
+                guesses = 5;
+                wordObject = new Word(animals[Math.floor(Math.random()*animals.length)])
+                wordObject.makeAndPushLettersIntoWord();
+                wordObject.updateLetter(data.guess);
+                console.log(" ");
+                console.log("Game restarted because you ran out of guesses, here if your new word");
+                console.log("Guesses left: " + guesses);
+                console.log(wordObject.display());
+                askLetter();
+            }
+
         } else if (data.guess == 'restart') {
             guesses = 5;
             wordObject = new Word(animals[Math.floor(Math.random()*animals.length)])
             wordObject.makeAndPushLettersIntoWord();
             wordObject.updateLetter(data.guess);
-            console.log(guesses + " guesses left " + wordObject.display());
-            askLetter();
-        }
-
-        if (guesses == 0) {
-            guesses = 5;
-            wordObject = new Word(animals[Math.floor(Math.random()*animals.length)])
-            wordObject.makeAndPushLettersIntoWord();
-            wordObject.updateLetter(data.guess);
+            console.log(" ");
+            console.log("Game restarted");
             console.log(guesses + " guesses left " + wordObject.display());
             askLetter();
         }
